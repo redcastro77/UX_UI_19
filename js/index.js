2ee2290b645a11d93e9ffa9cc9d256b4920c9d29
redcastro77 <101912646+redcastro77@users.noreply.github.com>
2022-04-16 15:09:01 -0600

    smileChange

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("My index.js file is loaded correctly. Yay!");
+
+$(document).ready(function(){
+    $("rSmile").hover(function(){
+      $(this).;
+      }, function(){
+      $(this).css("background-color", "pink");
+    });
+  });
